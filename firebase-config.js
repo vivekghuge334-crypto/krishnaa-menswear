aca1ba7dd4920b1e6fdaf4c5be629fe70d4744a7
vivekghuge334-crypto <vivekghuge334@gmail.com>
2026-02-28 21:06:25 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB9gYDFOtNuoy_xDtZAB8VhqO_SYBs2kUQ",
+  authDomain: "krushnamenswear009.firebaseapp.com",
+  projectId: "krushnamenswear009",
+  storageBucket: "krushnamenswear009.appspot.com"
+};
