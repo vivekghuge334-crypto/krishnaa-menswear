4eda8cc844b6b1683d6dbc4d302fa28bd16e0e0f
vivekghuge334-crypto <vivekghuge334@gmail.com>
2026-03-01 12:04:02 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,7 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyB9gYDFOtNuoy_xDtZAB8VhqO_SYBs2kUQ",
-  authDomain: "krushnamenswear009.firebaseapp.com",
-  projectId: "krushnamenswear009",
-  storageBucket: "krushnamenswear009.appspot.com"
-};
